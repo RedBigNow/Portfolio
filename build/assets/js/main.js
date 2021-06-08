b4b238d303681a239ed62895c7648fc2177806d2
Nikita <nikita_sokolov98@mail.ru>
2021-06-08 22:23:13 +0300

    Главная страница без адаптива

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -5,4 +5,27 @@ $(window).on('load resize', function () {
   var heightWrapper = document.querySelector(".wrapper").offsetHeight;
   document.querySelector(".bg-line").style.height = heightWrapper + "px";
 });
-$(document).ready(function () {});
+$(document).ready(function () {
+  var heroSlider = new Swiper('.hero-slider__container', {
+    slidesPerView: 1,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    autoplay: {
+      delay: 10000
+    }
+  }); //Вывод общего кол-ва слайдов
+
+  var SlidesTotal = $('.portfolio-slider__item').length;
+  $(".total").text(SlidesTotal); //Вывод номера текущего слайда
+
+  heroSlider.on('slideChange', function () {
+    $(".current").text(heroSlider.realIndex + 1);
+    console.log(heroSlider.realIndex);
+  });
+});
--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -4,8 +4,33 @@ $(window).on('load resize', function() {
     document.querySelector(".bg-line").style.height = heightWrapper + "px";
 });
 
+
 $(document).ready(function () {
 
+    const heroSlider = new Swiper('.hero-slider__container', {
+        slidesPerView: 1,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        autoplay: {
+			delay: 10000
+		}
+      });
+      
+    //Вывод общего кол-ва слайдов
+    var SlidesTotal = $('.portfolio-slider__item').length;
+    $(".total").text(SlidesTotal);
+
+    //Вывод номера текущего слайда
+    heroSlider.on('slideChange', function () {
+        $(".current").text(heroSlider.realIndex +1);
+        console.log(heroSlider.realIndex);
+    });
 
 });
 
